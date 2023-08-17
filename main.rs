6163dfdeb6cf2b5d708e30cc5b2821b14b283f05
mekosko <135838138+mekosko@users.noreply.github.com>
2023-08-18 01:42:16 +0500

    Remove debug code

--- main.rs
+++ main.rs
@@ -9,13 +9,10 @@ async fn main() {
 	let environment = std::fs::read_to_string("m3u8.json").unwrap();
 	let environment = std::sync::Arc::new(Environment::from(environment));
 
-	println!("{:?}", environment);
-
 	let app = Router::new().route("/", get(executor::get)).with_state(environment.clone());
 
 	let out = axum::Server::bind(&environment.socket).serve(app.into_make_service()).await;
 
 	out.unwrap();
-
 	()
 }
