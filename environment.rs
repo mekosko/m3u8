ae81bd64c0699dc3783e414ade7c6b7563b91280
mekosko <135838138+mekosko@users.noreply.github.com>
2023-08-18 00:38:21 +0500

    Rework environment

--- environment.rs
+++ environment.rs
@@ -1,13 +1,50 @@
-use {serde::Deserialize, std::net::SocketAddr};
+use std::net::SocketAddr;
 
-#[derive(Debug, Deserialize)]
+#[derive(serde::Deserialize)]
+struct RawData {
+	socket: Vec<String>,
+	locate: Vec<String>,
+	target: Vec<String>,
+}
+
+#[derive(Debug)]
 pub struct Environment {
+	pub socket: SocketAddr,
 	pub locate: String,
-	pub at: SocketAddr,
+	pub target: String,
 }
 
 impl From<String> for Environment {
 	fn from(value: String) -> Self {
-		serde_json::from_str(value.as_str()).unwrap()
+		let data = serde_json::from_str::<RawData>(value.as_str()).unwrap();
+
+		let socket = format!("{}:{}", data.socket[0], data.socket[1]).parse().unwrap();
+
+		let mut locate = String::new();
+		let mut components = data.locate.into_iter();
+
+		locate.push_str(components.next().unwrap().as_str());
+		locate.push_str("://");
+
+		for i in components {
+			locate.push_str(i.as_str());
+			locate.push('/')
+		}
+
+		let mut target = String::new();
+		let mut components = data.target.into_iter();
+
+		(components.next().unwrap() == "1").then(|| target.push('/'));
+
+		for i in components {
+			target.push_str(i.as_str());
+			target.push('/')
+		}
+
+		Self {
+			socket,
+			locate,
+			target,
+		}
 	}
 }
--- executor.rs
+++ executor.rs
@@ -3,5 +3,11 @@ use axum::{extract::State, http::StatusCode, response::IntoResponse};
 type Environment = std::sync::Arc<crate::environment::Environment>;
 
 pub async fn get(State(environment): State<Environment>) -> Result<impl IntoResponse, StatusCode> {
+	let paths = std::fs::read_dir("template").unwrap();
+
+	for path in paths {
+		println!("{:?}", path.unwrap());
+	}
+
 	Ok(format!("{:?}", environment))
 }
